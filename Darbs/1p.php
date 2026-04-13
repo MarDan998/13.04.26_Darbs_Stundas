e221a2fafbb53922f8854baa1df64aafc5604267
Markuss <markusssoms1@gmail.com>
2026-04-13 08:51:30 +0000

    Added category handing improvements

--- Darbs/1p.php
+++ Darbs/1p.php
@@ -2,37 +2,34 @@
 
 $notes = [];
 
-while (true) {
-    echo "Ievadi piezimes nosaukumu: ";
-    
-    $title = trim(fgets(STDIN));
+function addNote(&$notes) {
+$title = readline("Piezimes nosaukums: ");
+$category = readline("Piezimes kategorija: ");
+$description = readline("Piezimes apraksts: ");
 
-    echo "Ievadi kategoriju: ";
-    $category = trim(fgets(STDIN));
-
-    echo "Ievadi aprkastku:";
-    $description = trim(fgets(STDIN));
-
-    $notes[] = [
-        'title' => $title,
-        'category' => $category,
-        'description' => $description
-    ];
-
-    echo "Vai vēlaties pievienot vēl vienu piezīmi? (jā/nē): ";
-    $answer = trim(fgets(STDIN));
-    if ($answer !== 'jā') {
-        break;
-
+$notes[] = [
+    'title' => $title,
+    'category' => $category,
+    'description' => $description
+];
+echo "Vai velaties pievienot vel vienu piezimi? (j/n): ";
+$answer = readline();
+if (strtolower($answer) === 'j') {
+    addNote($notes);
+}
+}
+function showNotes($notes) {
+    echo "\nPiezimes:\n";
+    foreach ($notes as $note) {
+        echo "Nosaukums:";
+        $note["title"] . "\n";
+        $note["category"] . "\n";
+        $note["description"] . "\n";
+        echo "-------------------\n";
     }
 }
-echo "\n VISAS PIEZIMES \n";
-foreach ($notes as $index => $note) {
-    echo "Nosaukums:";
-    $note['title'] . "\n";
-    echo "Kategorija:";
-    $note['category'] . "\n";
-    echo "Apraksts:";
-    $note['description'] . "\n";
-    echo "-------------------------\n";
-}
+echo "piezimju sistema\n";
+addNote($notes);
+showNotes($notes);
+
+?>
